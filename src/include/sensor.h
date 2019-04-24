e38bd146353b4290e67e7a2c35b32404c2703ecb
bumsik <bumdori2@gnu.ac.kr>
2019-04-24 14:32:14 +0900

    draft

--- src/include/sensor.h
+++ src/include/sensor.h
@@ -5,20 +5,17 @@
 
 
 typedef struct _sensor
-{
-	pthread_mutex_t lock;
+{//굳이 쓰레드를 써야할까 ..
+	pthread_mutex_t sensor_lock;
 	struct list buffer_head;
+	int device_addr;
 	struct comm_device* com;
-	
 	struct list_elem* (*read_buffer)(struct _sensor*);
 	/* 	args: self
 		returns: next element's pointer
 		doing: read buffer and remove node from list
 	*/
-	struct list_elem* (*write_buffer)(struct _sensor*, struct list_elem*);
-	
-	size_t (*read)(struct _sensor*, char*);
-	void (*write)(struct _sensor*, char*, size_t);
+	struct list_elem* (*write_buffer)(struct _sensor*, struct list_elem*);	
 }sensor; 
 	
 	
