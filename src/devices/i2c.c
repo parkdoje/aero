3ef176c76a27ec3aea99daee5ab7c73e6affc979
bumsik <bumdori2@gnu.ac.kr>
2019-05-20 17:42:24 +0900

    comment

--- src/devices/i2c.c
+++ src/devices/i2c.c
@@ -7,7 +7,7 @@
 #include "../include/i2c.h"
 
 
-
+// i2c initializer
 i2c_dev_t* init_i2c(const char* device_name)
 {
 	int fd = open_device(device_name);
@@ -50,7 +50,7 @@ i2c_dev_t* init_i2c(const char* device_name)
 	return self;
 
 }
-
+//set device addr, after set, every write read procedure will perform to addr
 int set_address(i2c_dev_t* self, int addr)
 {
 	ASSERT(self->super.type == I2C);
@@ -60,12 +60,14 @@ int set_address(i2c_dev_t* self, int addr)
 	self->dev_addr = addr;
 	return 0;
 }
+// get addr of current device
 uint8_t get_address(i2c_dev_t* self)
 {
 	ASSERT(self->super.type = I2C);
 	return ((i2c_dev_t*)self)->dev_addr;
 }
 
+//internal function don't use
 int open_device(const char* device_name)
 {
 	int fd;
@@ -77,13 +79,14 @@ int open_device(const char* device_name)
 	return fd;
 }
 
+//destructor
 int close_device(i2c_dev_t* _self)
 {
 	ASSERT(_self->super.type = I2C);
 	i2c_dev_t* self = (i2c_dev_t*)_self;
 	free(self);
 }
-
+//internal fucntion
 static int check_functions(int fd, unsigned int func_code)
 {
 	unsigned long funcs;
@@ -94,7 +97,7 @@ static int check_functions(int fd, unsigned int func_code)
 	return (func_code & funcs);
 }
 
-
+//internal fucntion
 static inline int i2c_access(int fd, char rw, uint8_t command, int size, union i2c_smbus_data* data)
 {
 	struct i2c_smbus_ioctl_data args;
@@ -105,7 +108,7 @@ static inline int i2c_access(int fd, char rw, uint8_t command, int size, union i
 	args.data = data;
 	return ioctl(fd, I2C_SMBUS, &args);
 }
-
+//read byte from device, don't care about register
 int i2c_read_byte(i2c_dev_t* self)
 {
 	
@@ -115,6 +118,7 @@ int i2c_read_byte(i2c_dev_t* self)
 	return data.byte & 0xFF;
 }
 
+//write byte to device, not a register
 int i2c_write_byte(i2c_dev_t* self, uint8_t value)
 {
 	union i2c_smbus_data data;
@@ -123,6 +127,8 @@ int i2c_write_byte(i2c_dev_t* self, uint8_t value)
 	return i2c_access(self->super.fd, I2C_SMBUS_WRITE, value, I2C_SMBUS_BYTE, NULL);
 }
 
+
+//read byte from device's register
 int i2c_read_byte_reg(i2c_dev_t* self, uint8_t reg)
 {
 	union i2c_smbus_data data;
@@ -131,6 +137,7 @@ int i2c_read_byte_reg(i2c_dev_t* self, uint8_t reg)
 	return data.byte & 0xFF; // why do we have to do this?
 }
 
+// read 2 byte from register, len must 2
 int i2c_read_nbyte_reg(i2c_dev_t* self, uint8_t reg, size_t len, uint8_t* buffer)
 {
 	union i2c_smbus_data data;
@@ -140,7 +147,7 @@ int i2c_read_nbyte_reg(i2c_dev_t* self, uint8_t reg, size_t len, uint8_t* buffer
 	memcpy(buffer, &data.word, 2);
 	return 2;
 }
-
+//write byte to device's register
 int i2c_write_byte_reg(i2c_dev_t* self, uint8_t reg, uint8_t data)
 {
 	union i2c_smbus_data packet;
@@ -161,8 +168,6 @@ int i2c_write_nbyte_reg(i2c_dev_t* self, uint8_t reg, size_t len, uint8_t* buffe
 
 	return i2c_access(self->super.fd, I2C_SMBUS_WRITE, reg, I2C_SMBUS_BLOCK_DATA, &packet);
 }
-
-
 
 int i2c_write_bit_reg(i2c_dev_t* self, uint8_t reg, uint8_t pos, uint8_t len, uint8_t data, bool mode)
 {
