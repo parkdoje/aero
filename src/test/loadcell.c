1c6fb8705b72782b7433ef6068dd9c422da083f1
bumsik <bumdori2@gnu.ac.kr>
2019-05-29 16:16:59 +0900

    1st implemetation of loadcell testcode

--- src/test/loadcell.c
+++ src/test/loadcell.c
@@ -5,69 +5,83 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <signal.h>
 
 serial_dev_t* uart = NULL;
 FILE* result_file = NULL;
+static int intflag = 0;
 
 
 int read_packet(uint8_t buf[static 64])
 {
     int length = 0 , received = 0; 
     memset(buf, 0x00, 64);
-    while(length < 22)
+    while(length < 23 && ((received = uart->super.read_byte(uart)) != 0x0a))
     {
-        received = uart->super.read_byte(uart);
         if(received < 0)
-            continue;
-        buf[length] = received;
-        length +=1;
+        {
+           continue;
+        }
+        buf[length++] = received;
     }
-    buf[length + 1] = 0x00;
 
-    if(buf[length] == 0x0A)
-        return 0;
-    
+    buf[length] = 0x00;
+
     return -1;
 }
 
 void parse_packet(uint8_t buf[static 64])
 {
+    struct timespec ts;
     char* cur = NULL, *saved;
     double a;
     int i = 0;
     cur = __strtok_r(buf, ",", &saved);
     do
     {
-        a = atof(cur);
-        if (a == 0) 
+        if(i == 3)
         {
-            continue;
+            a = atof(cur);
+            clock_gettime(CLOCK_MONOTONIC, &ts);
+            fprintf(result_file, "%d, %d, %f\n", ts.tv_sec, ts.tv_nsec / 1000000, a);// sec, ms, kgf 
+            
+            break;
         }
-        printf("%f\n", a);
-
+        i++;
+        printf("Test %s\n",cur);
     }
     while((cur = __strtok_r(NULL, ",", &saved)) != NULL);
+    
+
+}
+
+int shutdown(int sig)
+{
+    intflag = 1;
 }
 
 int main(int argc, char* argv[])
 {
-    // 1st arg = /dev/something, 2nd arg = baud rate
+    // 1st arg = /dev/something, 2nd arg = baud rate, result_file_name
 
-    struct timespec ts;
-    uint8_t buf[64];
-    memcpy(buf, argv[3], 16);
-    //printf("%s\n", argv[3]);
-    parse_packet(buf);
-
-    int len = 0;
-
-
-    clock_gettime(CLOCK_REALTIME_COARSE, &ts);
-    int start = ts.tv_sec;   
-    int cur = 0;
-    memset(buf, 0x00, 64);
-    //uart->data_flush(uart);
-    uint8_t data = 0;
+    init_serial(argv[1], atoi(argv[2]));
+    printf("%s is dev interface , %s is baudrate\n", argv[1], argv[2]);
+    uint8_t buf[64] = {0, };
     
+    signal(SIGINT, shutdown);
+    result_file = fopen(argv[3], "a+");
+
+    memcpy(buf, argv[4], sizeof(buf));
+
+    uart->data_flush(uart);
+    while(!intflag)
+    {
+        read_packet(buf);
+        parse_packet(buf);
+        memset(buf, 0x00, sizeof(buf));
+    }
+
+    fclose(result_file);
+
     return 0;
 }
